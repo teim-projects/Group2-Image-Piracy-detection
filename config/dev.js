fffa5a18dcffb25bb1b402c1f0341555531cc79e
teim-projects <108449533+teim-projects@users.noreply.github.com>
2023-05-23 02:32:33 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports={
+    googleClientID:"726096484264-7orqhqaq0m97gnmu0gr1pddssfjcv0vq.apps.googleusercontent.com",
+    googleClientSecret: "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",
+    mongoURI:"mongodb+srv://anushkashrivastava:password1234@cluster0.hjp1eu2.mongodb.net/?retryWrites=true&w=majority",
+    cookieKey:"Ladidaaaaaa",
+    imgurID:process.env.IMGUR_ID
+}
